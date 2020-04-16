1d3119f9c3ca0eca5f63ff1f64d17901e358ef1d
Naseem Mohammed <m.naseem@outlook.com>
2020-04-16 14:26:36 +0000

    Deleting

--- controlpass.ps1
+++
@@ -1,6 +0,0 @@
-
-az aks get-credentials --name malxxdtax -g OndfimRG --admin
-
-az aks create --resource-group oneSimRG --name malter --generate-ssh-keys --aad-server-app-id 349356c2-7859f --aad-server-app-secret /Sa?XLj2igp10C@x --aad-client-app-id ce4b1fd8080726527180 --aad-tenant-id 5d528a70b3d1a65b7 --service-principal 5301fae07f8b2 --client-secret Q3ATTaUQEkar --node-count=2
- 
-
